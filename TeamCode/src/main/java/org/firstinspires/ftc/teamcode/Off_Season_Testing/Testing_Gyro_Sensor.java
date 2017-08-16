1234f02ebe2d897a1d1cda2f442f394ab972f8a6
LauraAEvans <elzagirl10@gmail.com>
2017-08-15 20:44:37 -0500

    Looked into the IMU Sensor on the Rev Module

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Off_Season_Testing/Testing_Gyro_Sensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Off_Season_Testing/Testing_Gyro_Sensor.java
@@ -1,238 +1,162 @@
 package org.firstinspires.ftc.teamcode.Off_Season_Testing;
 
-
-import android.support.annotation.NonNull;
-
 import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import com.qualcomm.hardware.bosch.BNO055IMU.SensorMode;
-import com.qualcomm.hardware.bosch.BNO055IMU.SystemStatus;
-import com.qualcomm.robotcore.hardware.I2cDevice;
-
+import org.firstinspires.ftc.robotcore.external.Func;
 import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.AngularVelocity;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
-import org.firstinspires.ftc.robotcore.external.navigation.MagneticFlux;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.robotcore.external.navigation.Position;
-import org.firstinspires.ftc.robotcore.external.navigation.Quaternion;
-import org.firstinspires.ftc.robotcore.external.navigation.TempUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Temperature;
 import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
 
+import java.util.Locale;
 
 /**
- * Created by User on 7/29/2017.
+ * {@link Testing_Gyro_Sensor} gives a short demo on how to use the BNO055 Inertial Motion Unit (IMU) from AdaFruit.
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ *
+ * @see <a href="http://www.adafruit.com/products/2472">Adafruit IMU</a>
  */
+@Autonomous(name = "Sensor: BNO055 IMU", group = "Sensor")
 
-@TeleOp(name = "RevGyroTest", group = "Testing")
 public class Testing_Gyro_Sensor extends LinearOpMode
 {
+   //----------------------------------------------------------------------------------------------
+   // State
+   //----------------------------------------------------------------------------------------------
 
-   BNO055IMU thing = new BNO055IMU()
+   // The IMU sensor object
+   BNO055IMU imu;
 
-   {
-      @Override
-      public boolean initialize(@NonNull Parameters parameters)
-      {
-         return false;
-      }
+   // State used for updating telemetry
+   Orientation angles;
+   Acceleration gravity;
+   double temperature;
 
-      @NonNull
-      @Override
-      public Parameters getParameters()
-      {
-         return null;
-      }
+   //----------------------------------------------------------------------------------------------
+   // Main logic
+   //----------------------------------------------------------------------------------------------
 
-      @Override
-      public void close()
-      {
+   @Override public void runOpMode() {
 
-      }
+      // Set up the parameters with which we will use our IMU. Note that integration
+      // algorithm here just reports accelerations to the logcat log; it doesn't actually
+      // provide positional information.
+      BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+      parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+      parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+      parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+      parameters.loggingEnabled      = true;
+      parameters.loggingTag          = "IMU";
+      parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+      parameters.temperatureUnit     = BNO055IMU.TempUnit.CELSIUS;
 
-      @Override
-      public Orientation getAngularOrientation()
-      {
-         return null;
-      }
+      // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
+      // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
+      // and named "imu".
+      imu = hardwareMap.get(BNO055IMU.class, "imu");
+      imu.initialize(parameters);
 
-      @Override
-      public Orientation getAngularOrientation(AxesReference reference, AxesOrder order, org.firstinspires.ftc.robotcore.external.navigation.AngleUnit angleUnit)
-      {
-         return null;
-      }
+      // Set up our telemetry dashboard
+      composeTelemetry();
 
-      @Override
-      public Acceleration getOverallAcceleration()
-      {
-         return null;
-      }
-
-      @Override
-      public AngularVelocity getAngularVelocity()
-      {
-         return null;
-      }
-
-      @Override
-      public Acceleration getLinearAcceleration()
-      {
-         return null;
-      }
-
-      @Override
-      public Acceleration getGravity()
-      {
-         return null;
-      }
-
-      @Override
-      public Temperature getTemperature()
-      {
-         return this.getTemperature();
-      }
-
-      @Override
-      public MagneticFlux getMagneticFieldStrength()
-      {
-         return null;
-      }
-
-      @Override
-      public Quaternion getQuaternionOrientation()
-      {
-         return null;
-      }
-
-      @Override
-      public Position getPosition()
-      {
-         return null;
-      }
-
-      @Override
-      public Velocity getVelocity()
-      {
-         return null;
-      }
-
-      @Override
-      public Acceleration getAcceleration()
-      {
-         return null;
-      }
-
-      @Override
-      public void startAccelerationIntegration(Position initialPosition, Velocity initialVelocity, int msPollInterval)
-      {
-
-      }
-
-      @Override
-      public void stopAccelerationIntegration()
-      {
-
-      }
-
-      @Override
-      public SystemStatus getSystemStatus()
-      {
-         return null;
-      }
-
-      @Override
-      public SystemError getSystemError()
-      {
-         return null;
-      }
-
-      @Override
-      public CalibrationStatus getCalibrationStatus()
-      {
-         return null;
-      }
-
-      @Override
-      public boolean isSystemCalibrated()
-      {
-         return false;
-      }
-
-      @Override
-      public boolean isGyroCalibrated()
-      {
-         return false;
-      }
-
-      @Override
-      public boolean isAccelerometerCalibrated()
-      {
-         return false;
-      }
-
-      @Override
-      public boolean isMagnetometerCalibrated()
-      {
-         return false;
-      }
-
-      @Override
-      public CalibrationData readCalibrationData()
-      {
-         return null;
-      }
-
-      @Override
-      public void writeCalibrationData(CalibrationData data)
-      {
-
-      }
-
-      @Override
-      public byte read8(Register register)
-      {
-         return 0;
-      }
-
-      @Override
-      public byte[] read(Register register, int cb)
-      {
-         return new byte[0];
-      }
-
-      @Override
-      public void write8(Register register, int bVal)
-      {
-
-      }
-
-      @Override
-      public void write(Register register, byte[] data)
-      {
-
-      }
-
-   };
-
-   public void runOpMode()
-   {
-
+      // Wait until we're told to go
       waitForStart();
 
-      while(opModeIsActive())
-      {
-         thing.getTemperature();
-         telemetry.addData("Temperature?", thing.getTemperature().toUnit(TempUnit.CELSIUS));
+      // Start the logging of measured acceleration
+      imu.startAccelerationIntegration(new Position(), new Velocity(), 1000);
+
+      // Loop and update the dashboard
+      while (opModeIsActive()) {
          telemetry.update();
       }
-
    }
+
+   //----------------------------------------------------------------------------------------------
+   // Telemetry Configuration
+   //----------------------------------------------------------------------------------------------
+
+   void composeTelemetry() {
+
+      // At the beginning of each telemetry update, grab a bunch of data
+      // from the IMU that we will then display in separate lines.
+      telemetry.addAction(new Runnable() { @Override public void run()
+      {
+         // Acquiring the angles is relatively expensive; we don't want
+         // to do that in each of the three items that need that info, as that's
+         // three times the necessary expense.
+         angles   = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+         gravity  = imu.getGravity();
+         temperature = imu.getTemperature().temperature;
+
+      }
+      });
+
+
+      telemetry.addLine()
+              .addData("status", new Func<String>() {
+                 @Override public String value() {
+                    return imu.getSystemStatus().toShortString();
+                 }
+              })
+              .addData("calib", new Func<String>() {
+                 @Override public String value() {
+                    return imu.getCalibrationStatus().toString();
+                 }
+              });
+
+      telemetry.addLine()
+              .addData("heading", new Func<String>() {
+                 @Override public String value() {
+                    return formatAngle(angles.angleUnit, angles.firstAngle);
+                 }
+              })
+              .addData("roll", new Func<String>() {
+                 @Override public String value() {
+                    return formatAngle(angles.angleUnit, angles.secondAngle);
+                 }
+              })
+              .addData("pitch", new Func<String>() {
+                 @Override public String value() {
+                    return formatAngle(angles.angleUnit, angles.thirdAngle);
+                 }
+              });
+
+      telemetry.addLine()
+              .addData("grvty", new Func<String>() {
+                 @Override public String value() {
+                    return gravity.toString();
+                 }
+              })
+              .addData("mag", new Func<String>() {
+                 @Override public String value() {
+                    return String.format(Locale.getDefault(), "%.3f",
+                            Math.sqrt(gravity.xAccel*gravity.xAccel
+                                    + gravity.yAccel*gravity.yAccel
+                                    + gravity.zAccel*gravity.zAccel));
+                 }
+              });
+      telemetry.addData("Temperature", temperature);
 }
 
+   //----------------------------------------------------------------------------------------------
+   // Formatting
+   //----------------------------------------------------------------------------------------------
 
+   String formatAngle(AngleUnit angleUnit, double angle) {
+      return formatDegrees(AngleUnit.DEGREES.fromUnit(angleUnit, angle));
+   }
+
+   String formatDegrees(double degrees){
+      return String.format(Locale.getDefault(), "%.1f", AngleUnit.DEGREES.normalize(degrees));
+   }
+}
