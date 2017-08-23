25feb223837577b6fc27473c278e5585541c7371
LauraAEvans <elzagirl10@gmail.com>
2017-08-22 20:46:25 -0500

    -Added functionality to read a rev color sensor/proximity sensor in the RevModuleRobotTesting java file (intended to run on 2nd rev module)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Off_Season_Testing/RevModuleRobotTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Off_Season_Testing/RevModuleRobotTesting.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.Off_Season_Testing;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 /**
@@ -15,12 +16,16 @@ public class RevModuleRobotTesting extends OpMode
     DcMotor motorL;
     DcMotor motorLF;
 
+    ColorSensor revCSensor;
+
     public void init()
     {
         motorRF = hardwareMap.dcMotor.get("motorRF");
         motorR = hardwareMap.dcMotor.get("motorR");
         motorLF = hardwareMap.dcMotor.get("motorLF");
         motorL = hardwareMap.dcMotor.get("motorL");
+
+        revCSensor = hardwareMap.colorSensor.get("revCSensor");
 
         motorRF.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         motorLF.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -35,6 +40,15 @@ public class RevModuleRobotTesting extends OpMode
         motorLF.setPower(gamepad1.left_stick_y);
         motorL.setPower(gamepad1.left_stick_y);
 
+        telemetry.addData("Red", revCSensor.red());
+        telemetry.addData("Green", revCSensor.green());
+        telemetry.addData("Blue", revCSensor.blue());
+
+        telemetry.addData("argb", revCSensor.argb());
+
+        telemetry.addData("revCSensor Hash Code", revCSensor.hashCode());
+
+        telemetry.update();
 
     }
 
