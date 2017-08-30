c1b658cc02694215ddf93a88ac913239bf57c518
LauraAEvans <elzagirl10@gmail.com>
2017-08-29 20:44:26 -0500

    -Modified how you control the servo -Made it so you use the left and right d-pad buttons to increase and decrease the servo's position -Added hard-coded variable range clip for the servoPos variable (0.0 to 1.0)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Off_Season_Testing/RevModuleRobotTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Off_Season_Testing/RevModuleRobotTesting.java
@@ -4,6 +4,8 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
 
 /**
  * Created by User on 7/18/2017.
@@ -18,6 +20,14 @@ public class RevModuleRobotTesting extends OpMode
 
     ColorSensor revCSensor;
 
+    Servo servo;
+
+    boolean leftDPadState = false;
+    boolean  rightDPadState = false;
+
+    double servoPos = 0.5;
+
+
     public void init()
     {
         motorRF = hardwareMap.dcMotor.get("motorRF");
@@ -27,10 +37,14 @@ public class RevModuleRobotTesting extends OpMode
 
         revCSensor = hardwareMap.colorSensor.get("revCSensor");
 
+        servo = hardwareMap.servo.get("servo");
+
         motorRF.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         motorLF.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         motorR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         motorL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        servo.setPosition(servoPos);
     }
 
     public void loop()
@@ -40,13 +54,55 @@ public class RevModuleRobotTesting extends OpMode
         motorLF.setPower(gamepad1.left_stick_y);
         motorL.setPower(gamepad1.left_stick_y);
 
+        /*
+        if(gamepad1.a)
+        {
+            servo.setPosition(0.0);
+        }
+        else if(gamepad1.b)
+        {
+            servo.setPosition(1.0);
+        }
+        */
+
+        if (gamepad1.dpad_left && !leftDPadState)
+        {
+            leftDPadState = true;
+            servoPos += 0.05;
+        }
+        else if (!gamepad1.dpad_left)
+        {
+            leftDPadState = false;
+        }
+
+        if (gamepad1.dpad_right && !rightDPadState)
+        {
+            rightDPadState = true;
+            servoPos -= 0.05;
+        }
+        else if (!gamepad1.dpad_right)
+        {
+            rightDPadState = false;
+        }
+
+        if (servoPos > 1.0)
+        {
+            servoPos = 1.0;
+        }
+        else if (servoPos < 0.0)
+        {
+            servoPos = 0.0;
+        }
+
+        servo.setPosition(servoPos);
+
         telemetry.addData("Red", revCSensor.red());
         telemetry.addData("Green", revCSensor.green());
         telemetry.addData("Blue", revCSensor.blue());
 
         telemetry.addData("argb", revCSensor.argb());
 
-        telemetry.addData("revCSensor Hash Code", revCSensor.hashCode());
+        telemetry.addData("Servo Position", servo.getPosition());
 
         telemetry.update();
 
